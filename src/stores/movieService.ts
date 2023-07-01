cc0053e36525e14ae306b0410203318dea96d187
git-Alp <alperen.aslan@windowslive.com>
2023-07-01 18:13:57 +0300

    movie data

--- src/stores/movieService.ts
+++ src/stores/movieService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 import {v4 as uuidv4} from 'uuid'
+import { differenceInSeconds, differenceInHours, isSameYear, format, formatDistance } from 'date-fns'
 
 const apiKey = import.meta.env.VITE_MOVIE_API_KEY
 
@@ -14,7 +15,66 @@ function topRated() {
   return api().get('top_rated?api_key=' + apiKey)
 }
 
-type Item = {
+export function handleUsername (val: string) {
+  return '@' + val.replace(/['\.,?!: ]/g,'')
+}
+
+export function handleDateFormat (val: string) {
+  console.log("val", val);
+  
+  const date = new Date()
+  const dataDate = new Date(val)
+  const secondsDifference = differenceInSeconds(date, dataDate)
+  const hoursDifference = differenceInHours(date, dataDate)
+  const checkYear = isSameYear(date, dataDate)
+  
+  const dateFormat = formatDistance(dataDate, date)
+
+  // let dateFormat = '';
+
+  // if (secondsDifference < 60) {
+  //   dateFormat = 'Now';
+  //   // console.log('Now');
+    
+  // } else if (hoursDifference < 1) {
+  //   dateFormat = format(dataDate, "m 'm'");
+  //   // console.log('hoursDifference', hoursDifference);
+  //   // console.log('date', date);
+  //   // console.log('dataDate', dataDate);
+
+  // } else if (hoursDifference > 1 && hoursDifference < 24) {
+  //   dateFormat = format(dataDate, "H 'h'");
+  //   // console.log('h', dateFormat);
+
+  // } else if (checkYear) {
+  //   dateFormat = format(dataDate, 'MMM i');
+  //   // console.log('MMM i', dateFormat);
+
+  // } else {
+  //   dateFormat = format(dataDate, 'PP');
+  //   // console.log('PP', dateFormat);
+
+  // }
+
+  return dateFormat
+}
+
+function designData (data: any) {
+  const movieData = 
+    data
+      .sort((objA: Item, objB: Item) => new Date(objB.release_date).valueOf() - new Date(objA.release_date).valueOf())
+      .map((movie: Item) => {
+        movie.id = uuidv4();
+        movie.user = handleUsername(movie.title)
+        // movie.release_date = handleDateFormat(movie.release_date);
+        
+        return movie;
+      })
+  
+  return movieData;
+}
+
+export type Item = {
   id: string;
   backdrop_path: string;
   title: string;
@@ -35,13 +95,7 @@ export const useMovieStore = defineStore('movie', {
       topRated()
         .then((movies) => {
           if (movies.data) {
-            const mapMovieData = movies.data.results.map((movie: Item) => {
-              const userName = movie.title.replace(/['\.,?!: ]/g,'')
-              movie.user = '@' + userName;
-              movie.id = uuidv4();
-              return movie;
-            });
-            this.movies = mapMovieData
+            this.movies = designData(movies.data.results)
           }
         })
         .catch(e => console.log("errör", e))
